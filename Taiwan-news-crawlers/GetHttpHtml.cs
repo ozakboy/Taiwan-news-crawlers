dc2f6fa254b477866b851b160a5b3c4cd19837db
ozakboy <ozakboy@gmail.com>
2023-02-09 12:46:08 +0800

    更新

---
+++ Taiwan-news-crawlers/GetHttpHtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taiwan_news_crawlers
+{
+	public static class GetHttpHtml
+	{
+		public static string GetHtml(string Url) 
+		{
+			HttpClient httpClient = new HttpClient();
+
+			//發送請求並取得回應內容
+			var responseMessage = httpClient.GetAsync(Url).Result;
+
+			//讀取Content內容
+			if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+				return responseMessage.Content.ReadAsStringAsync().Result;
+			else
+				return string.Empty;
+		}
+	}
+}
