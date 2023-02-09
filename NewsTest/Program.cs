835e23bc08330003bb5f049649b99defd309986a
ozakboy <ozakboy@gmail.com>
2023-02-09 14:44:52 +0800

    新增 中時

---
+++ NewsTest/Program.cs
@@ -0,0 +1,5 @@
+﻿using Taiwan_news_crawlers;
+using static Taiwan_news_crawlers.Chinatimes;
+
+Chinatimes _chinatimes = new Chinatimes();
+_chinatimes.GetNews(NewsType.Life);
---
+++ Taiwan-news-crawlers/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taiwan_news_crawlers
+{
+	public class News
+	{
+		public string Title { get; set; }
+		public string ContentBody { get; set; }
+
+		public string Author { get; set; }
+		public string Description { get; set; }
+		public string Url { get; set; }
+		public string UrlToImage { get; set; }
+		public string PublishedAt { get; set; }
+	}
+}
